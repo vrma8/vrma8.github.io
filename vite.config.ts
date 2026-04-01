03b67ab32a6f53a1e244089463cd4023ffc9c6a5
vrma8 <24bcs096@nith.ac.in>
2026-04-01 16:05:43 +0530

    changes in configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/vrma8.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
