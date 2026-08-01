0ce5b440a3980c04839924adc4b4b4dd315f8292
vrma8 <24bcs096@nith.ac.in>
2026-08-01 17:58:57 +0530

    refactor: update path alias resolution to use import.meta.dirname in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,9 @@
         '@radix-ui/react-aspect-ratio@1.1.2': '@radix-ui/react-aspect-ratio',
         '@radix-ui/react-alert-dialog@1.1.6': '@radix-ui/react-alert-dialog',
         '@radix-ui/react-accordion@1.2.3': '@radix-ui/react-accordion',
-        '@': path.resolve(__dirname, './src'),
+        '@': path.resolve(import.meta.dirname, './src'),
+
+
       },
     },
     build: {
